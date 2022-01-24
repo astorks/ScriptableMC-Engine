ad784f8f6ec0b62e1c88db0fcb6a912b75220873
Ashton Storks <ashton@pixlfox.com>
2022-01-24 16:03:59 -0700

    Updated Kotlin to v1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.jetbrains.kotlin.jvm") version "1.5.20-RC" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("org.jetbrains.gradle.plugin.idea-ext") version "1.0" apply false
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.1" apply false
 }
 
 var smcVersion = findProperty("smc.version")!!
